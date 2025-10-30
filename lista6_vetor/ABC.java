bf7aebd6b62c9c23d61a948b64d18b5d362d8fea
Gabriel Lima <limagabriel26122003@gmail.com>
2025-10-30 20:19:59 -0300

    exercicio6_vetores_atualizado

--- lista6_vetor/ABC.java
+++ lista6_vetor/ABC.java
@@ -1,10 +1,12 @@
-public class Indice {
+public class ABC {
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5,6,7,8,9,10};
         int[] b = new int[10];
+        int[] c = new int[10];
 
         for (int i = 0; i < a.length; i++) {
-            b[i] = a[i]*i;
+            b[i] = a[i];
+            c[i] = a[i]+b[i];
             System.out.println(a[i]);
         }
         System.out.println("-=-=".repeat(25));
@@ -15,6 +17,8 @@ public class Indice {
         }
         System.out.println("-=-=".repeat(25));
 
-       
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(c[i]);
+        }
     }
 }
